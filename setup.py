dd7be6a22a282225401eac7a5897acc6c88d954a
Kaizaburo Chubachi <8179588+zaburo-ch@users.noreply.github.com>
2022-11-30 20:43:44 +0900

    Use install_requires instead of requirements.txt

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
 from setuptools import find_packages, setup
 
-setup(name="somen", version="0.0.0", packages=find_packages("."), package_data={"somen": ["py.typed"]})
+setup(
+    name="somen",
+    version="0.0.0",
+    packages=find_packages("."),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "tables",
+        "torch",
+        "scikit-learn",
+        "pfio",
+        "albumentations",
+        "pytorch-pfn-extras",
+        "torchvision",
+        "feather-format",
+        "dacite",
+        "colorlog",
+        "tqdm",
+    ],
+    package_data={"somen": ["py.typed"]},
+)
